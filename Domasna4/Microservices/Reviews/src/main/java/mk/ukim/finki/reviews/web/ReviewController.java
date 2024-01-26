c7f3ae04aef308e18a8678177758d2869dfc55a7
sanjakrsteska <105121474+sanjakrsteska@users.noreply.github.com>
2024-01-26 01:43:09 +0100

    Update ReviewController.java

--- Domasna4/Microservices/Reviews/src/main/java/mk/ukim/finki/reviews/web/ReviewController.java
+++ Domasna4/Microservices/Reviews/src/main/java/mk/ukim/finki/reviews/web/ReviewController.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mk.ukim.finki.reviews.model.Review;
 import mk.ukim.finki.reviews.service.ReviewService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Slf4j
-@RequestMapping("/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
     private final ReviewService reviewService;
 
@@ -25,6 +25,21 @@ public class ReviewController {
         List<Review> reviewList = new ArrayList<>();
         reviewList = reviewService.allReviews(id);
         return reviewList;
+    }
+
+    @PostMapping("/add/winery/{id}")
+    public ResponseEntity<String> addReview(
+            @RequestParam Integer score,
+            @RequestParam String description,
+            @RequestParam(required = false) Long userId,
+            @PathVariable Long id) {
+
+        if (userId == null) {
+            return ResponseEntity.badRequest().body("Invalid user");
+        }
+
+        reviewService.addReview(score, description, id, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Review added successfully");
     }
 
 }
