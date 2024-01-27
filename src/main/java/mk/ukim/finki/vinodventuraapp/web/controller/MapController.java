6a18e6de4260ae7a1e8d24eb67cc1ba71318418a
Nikola <talevskinikola1409@gmail.com>
2024-01-27 17:14:53 +0100

    Trying to fix map error with map rendering json let part 3

--- src/main/java/mk/ukim/finki/vinodventuraapp/web/controller/MapController.java
+++ src/main/java/mk/ukim/finki/vinodventuraapp/web/controller/MapController.java
@@ -25,7 +25,7 @@ public class MapController {
 
         model.addAttribute("footerMap", "true");
         model.addAttribute("user",user);
-        model.addAttribute("wineries",wineries);
+        model.addAttribute("wineries",wineries.toString());
         String lang = (String)request.getSession().getAttribute("lang");
         if (lang.equals("mk")){
             model.addAttribute("bodyContent", "map-mk");
