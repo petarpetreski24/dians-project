a1a78c83b0291e32e6bfbb2b8f045f7677f8656c
Petar <petarpetreski24@gmail.com>
2024-01-28 13:51:34 +0100

    Deploying the final App

--- Domasna3/VinodventuraApp/src/main/java/mk/ukim/finki/vinodventuraapp/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.vinodventuraapp.service;
-
-import mk.ukim.finki.vinodventuraapp.model.User;
-
-public interface AuthService {
-    User login(String username, String password);
-    User register(String username, String password, String repeatPassword, String name, String surname);
-}
-
